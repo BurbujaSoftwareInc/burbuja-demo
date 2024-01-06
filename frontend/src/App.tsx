d2a758d09b9fd0bc3261ba7055b71f6ff8ecf63f
Spider-Gwen <maquinterov0399@gmail.com>
2024-01-06 14:44:25 -0600

    front modificado

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,35 +1,43 @@
-import { createSignal } from 'solid-js'
-import solidLogo from './assets/solid.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
-  const [count, setCount] = createSignal(0)
+   const [miembros, setMiembros] = useState("No hay miembros obtenidos");
+   async function llamaBackend() {
+      try {
+         const response = await axios.get("http://localhost:8000/equipo");
+         console.log(response);
+         const { data } = response; // {miembros: ""}
+         if (data.miembros && typeof data.miembros === "string") {
+            // Verificar la respuesta
+            setMiembros(data.miembros);
+         }
+      } catch (error) {
+         console.log("error");
+      }
+   }
 
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} class="logo" alt="Vite logo" />
-        </a>
-        <a href="https://solidjs.com" target="_blank">
-          <img src={solidLogo} class="logo solid" alt="Solid logo" />
-        </a>
-      </div>
-      <h1>Vite + Solid</h1>
-      <div class="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count()}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p class="read-the-docs">
-        Click on the Vite and Solid logos to learn more
-      </p>
-    </>
-  )
+   return (
+      <>
+         <div>
+            <a href="https://vitejs.dev" target="_blank">
+               <img src={viteLogo} className="logo" alt="Vite logo" />
+            </a>
+            <a href="https://react.dev" target="_blank">
+               <img src={reactLogo} className="logo react" alt="React logo" />
+            </a>
+         </div>
+         <h1>Vite + React</h1>
+         <div className="card">
+            <button onClick={async () => await llamaBackend()}>Obtener Miembros</button>
+            <p>{miembros}</p>
+         </div>
+         <p className="read-the-docs">Click on the Vite and React logos to learn more</p>
+      </>
+   );
 }
 
-export default App
+export default App;
